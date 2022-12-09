8b823bbbfe0b8a4a517746fdc097a9b3dbbc5250
srujana <gurrala.srujana@outlook.com>
2022-12-09 17:06:48 +0100

    songs list displayed

--- music-app/src/app/songs-list/songs-list.component.ts
+++ music-app/src/app/songs-list/songs-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-songs-list',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SongsListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+  
 
   ngOnInit(): void {
   }
+  data :any;
+  getData(){
+    this.httpClient.get('http://localhost:3000/api/v1/songs').subscribe((data) => {
+      this.data = data;
+            console.log(data);
+        });
+    }
 
 }
